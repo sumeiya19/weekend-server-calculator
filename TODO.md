1bdd07346f5ed0a45acaf8aebf8e26d60ab52721
Sumeiya Abdi <sumeiya619@gmail.com>
2024-05-11 23:57:36 -0500

    worked on addition operator

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# Tasks
+    - Create proper inputs
+    - Create functions for all buttons (+,-,*,/ and =)
+    - Client side: Display a list of previous calculations when the page loads (using a GET '/calculations' request)
+    - Create server routes
+        - GET /calculations that will send the calculations array back to the client
+        - POST /calculations that will "do the math" and obtain the correct result
--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,59 @@
-console.log('client.js is sourced!');
+let firstNumber = null;
+
+
+// Function for Plus Operator
+function plusOperator(event){
+    event.preventDefault();
+    console.log('plus works!');
+firstNumber = +document.getElementById("numberOne").value;
+console.log('First number check', firstNumber);
+
+}
+
+function subOperator(event){
+    event.preventDefault();
+    console.log('subtract works!');
+    firstNumber = +document.getElementById("numberOne").value;
+    console.log('First number check', firstNumber);
+}
+
+
+// Function for equal operation
+function getCalculations(event){
+event.preventDefault();
+console.log("getCalculations() works!" );
+if (firstNumber !== null) {
+    // Retrieve the second number from the input field
+    const secondNumber = +document.getElementById("numberTwo").value;
+    console.log('Second number check', secondNumber);
+    const result = addNumbers(firstNumber, secondNumber);
+    console.log('Result:', result);
+} else {
+    console.error('Please enter the first number.');
+}
+}
+
+
+
+
+function addNumbers(numberOne, numberTwo){
+    return numberOne + numberTwo;
+}
+
+function subNumbers(numberOne, numberTwo){
+    return numberOne + numberTwo;
+}
+
+function multiplyNumbers(numberOne, numberTwo){
+    return numberOne * numberTwo;
+}
+
+
+function divideNumbers(numberOne, numberTwo) {
+    // Check if the divisor is zero to prevent division by zero error
+    if (numberTwo === 0) {
+        throw new Error("Division by zero is not allowed.");
+    }
+
+    return numberOne / numberTwo;
+}
